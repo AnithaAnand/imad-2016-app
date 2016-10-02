21d43d97d65d3c10bb942b263d5144e8d2e8c3ed
AnithaAnand <anitha.subhashini@gmail.com>
2016-10-02 20:26:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,3 +21,21 @@ button.onclick = function (){
   request.open('GET', 'http://anithaanand.imad.hasura-app.io/counter', true);
   request.send(null);
   };
+  
+  //submit
+  var nameInput = document.getElementById('name');
+  var name = nameInput.value;
+  var submit = document.getElementById('submit_btn');
+  submit.onclick =function(){
+ //Make a request to the server and send the name
+ 
+ //capture a list of names and render it as list
+ var names =['name1', 'name2', 'name3'];
+ var list ='';
+ for (var i=0; i<names.length; i++){
+     list += '<li>' + names[i] + '</li>';
+ }
+   var ul = getElementById('namelist');
+   ul.innerHTML = list;
+  };
+  
