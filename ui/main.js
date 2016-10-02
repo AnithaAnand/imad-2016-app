84935ffc9851fef7a6bb70a940e73cdf9386e6b8
AnithaAnand <anitha.subhashini@gmail.com>
2016-10-02 21:18:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,19 +23,38 @@ button.onclick = function (){
   };
   
   //submit
-  var nameInput = document.getElementById('name');
-  var name = nameInput.value;
+  
   var submit = document.getElementById('submit_btn');
-  submit.onclick =function(){
- //Make a request to the server and send the name
  
- //capture a list of names and render it as list
- var names =['name1', 'name2', 'name3', 'name4'];
+  submit.onclick =function(){
+ 
+ //Make a request to the server and send the name
+  
+  var request = new XMLHttpRequest();
+  
+  //capture the response and store it in a variable
+  request.onreadystatechange = function(){
+     if (request.readyState === XMLHttprequest.DONE) {
+         //take some action 
+        if (request.status === 200){
+          var names =request.resposeText;
+          names = JSON.parse(names);
  var list ='';
  for (var i=0; i<names.length; i++){
      list += '<li>' + names[i] + '</li>';
  }
    var ul = getElementById('namelist');
-   ul.innerHTML = list;
+   ul.innerHTML = list;  
+        
+     }
+  }
   };
+  var nameInput = document.getElementById('name');
+  var name = nameInput.value;
+  //make the request
+  request.open('GET', 'http://anithaanand.imad.hasura-app.io/submit-name?name=' + name, true);
+  request.send(null);
+  
+ 
+ };
   
