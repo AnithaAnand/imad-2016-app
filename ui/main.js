a42042adb242cead94570f5979ff292fa9738d5a
AnithaAnand <anitha.subhashini@gmail.com>
2016-10-06 23:55:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -78,7 +78,7 @@ button.onclick = function (){
     };
  var commentInput = document.getElementById('comment');
  var comment = commentInput.value;
- request.send('GET', 'http://anithaanand.imad.hasura-app.io/comment-list?comment=' + comment, true);
+ request.open('GET', 'http://anithaanand.imad.hasura-app.io/comment-list?comment=' + comment, true);
   request.send(null);
   };
   
