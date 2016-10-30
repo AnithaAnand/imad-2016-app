5b9a8e1ac66f5df0a2f7e68fe1cc05811296728d
AnithaAnand <anitha.subhashini@gmail.com>
2016-10-30 16:54:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').Pool;
 
+var config = {
+    user: 'anithaanand',
+    database: 'anithaanand',
+    host: 'db.imad.hasura-app.io',
+    port:'5432',
+    password: process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 var articles = {
@@ -97,15 +105,23 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool = new Pool(config);
 app.get('/test-db', function (req, res){
     //make a select request
     //return a response with the results
+    pool.query('SELECT * FROM test', function(err, result) {
+        if (err) {
+            err.status(500).send(err.toString());
+        } else {
+            res.send(JSON.stringify(result));
+        }
+    });
 });
 
 var counter = 0;
 app.get('/counter',function (req,res){
     counter = counter + 1;
-    res.send(counter.toString())
+    res.send(counter.toString());
 });
 var names =[];
 app.get('/submit-name', function(req, res){
