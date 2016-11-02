23fae6b7fc147c44f757220da511b47a253f3988
AnithaAnand <anitha.subhashini@gmail.com>
2016-11-03 01:03:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -142,7 +142,7 @@ app.get('/comment-list', function(req, res){
 
 app.get('/articles/:articleName', function (req, res){
     var articleName = req.params.articleName;
-    pool.query("SELECT * FROM article WHERE title= " + req.params.articleName, function (err, result){
+    pool.query("SELECT * FROM article WHERE title= '" + req.params.articleName+"'", function (err, result){
       if (err) {
           err.status(500).send(err.toString());
       } else {
