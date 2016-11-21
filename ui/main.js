eb83ca63c0dd149e5b8eddc256a1cc16d5bed508
AnithaAnand <anitha.subhashini@gmail.com>
2016-11-21 23:27:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -53,7 +53,7 @@
   var password = document.getElementById('password').value;
   console.log(username);
   console.log(password);
-  request.open('POST', 'http://anithaanand.imad.hasura-app.io/login, true);
+  request.open('POST', 'http://anithaanand.imad.hasura-app.io/login', true);
   request.setRequestHeader('Content-Type', 'application/json');
   request.send(JSON.stringify({username: username, password: password}));
    };
